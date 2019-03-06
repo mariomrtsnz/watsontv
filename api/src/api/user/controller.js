81e934cb8234b10f131a100b86a196fd1dc6296b
Mario Martinez <mario.mrtsnz@gmail.com>
2019-03-06 12:25:50 +0100

    Started working on Auth and Basic User API calls

---
+++ api/src/api/user/controller.js
@@ -0,0 +1,135 @@
+import { success, notFound } from '../../services/response/'
+import { User } from '.'
+import { sign } from '../../services/jwt'
+
+export const index = ({ querymen: { query, select, cursor } }, res, next) =>
+  User.count(query)
+    .then(count => User.find(query, select, cursor)
+      .then(users => ({
+        rows: users.map((user) => user.view()),
+        count
+      }))
+    )
+    .then(success(res))
+    .catch(next)
+
+export const show = ({ params }, res, next) =>
+  User.findById(params.id)
+    .then(notFound(res))
+    .then((user) => user ? user.view() : null)
+    .then(success(res))
+    .catch(next)
+
+export const showMe = ({ user }, res) =>
+  res.json(user.view(true))
+
+export const create = ({ bodymen: { body } }, res, next) =>
+  User.create(body)
+    .then(user => {
+      sign(user.id)
+        .then((token) => ({ token, user: user.view(true) }))
+        .then(success(res, 201))
+    })
+    .catch((err) => {
+      /* istanbul ignore else */
+      if (err.name === 'MongoError' && err.code === 11000) {
+        res.status(409).json({
+          valid: false,
+          param: 'email',
+          message: 'email already registered'
+        })
+      } else {
+        next(err)
+      }
+    })
+
+export const update = ({ bodymen: { body }, params, user }, res, next) =>
+  User.findById(params.id === 'me' ? user.id : params.id)
+    .then(notFound(res))
+    .then((result) => {
+      if (!result) return null
+      const isAdmin = user.role === 'admin'
+      const isSelfUpdate = user.id === result.id
+      if (!isSelfUpdate && !isAdmin) {
+        res.status(401).json({
+          valid: false,
+          message: 'You can\'t change other user\'s data'
+        })
+        return null
+      }
+      return result
+    })
+    .then((user) => user ? Object.assign(user, body).save() : null)
+    .then((user) => user ? user.view(true) : null)
+    .then(success(res))
+    .catch(next)
+
+export const updatePassword = ({ bodymen: { body }, params, user }, res, next) =>
+  User.findById(params.id === 'me' ? user.id : params.id)
+    .then(notFound(res))
+    .then((result) => {
+      if (!result) return null
+      const isSelfUpdate = user.id === result.id
+      if (!isSelfUpdate) {
+        res.status(401).json({
+          valid: false,
+          param: 'password',
+          message: 'You can\'t change other user\'s password'
+        })
+        return null
+      }
+      return result
+    })
+    .then((user) => user ? user.set({ password: body.password }).save() : null)
+    .then((user) => user ? user.view(true) : null)
+    .then(success(res))
+    .catch(next)
+
+export const destroy = ({ params }, res, next) =>
+  User.findById(params.id)
+    .then(notFound(res))
+    .then((user) => user ? user.remove() : null)
+    .then(success(res, 204))
+    .catch(next)
+
+
+// CUSTOM CONTROLLERS
+
+export const getTotalWatchedTime = ({ params }, res, next) => {
+  User.findById(params.id)
+    .then(notFound(res))
+    .then((user) => user ? user.view() : null)
+    .then(success(res))
+    .catch(next)
+}
+
+export const editUserFriends = ({ params, user }, res, next) => {
+  const found = user.friends.indexOf(params.id);
+  if (found != -1)
+    user.friends.splice(found, 1);
+  else
+    user.friends.push(params.id);
+  user.save()
+    .then(success(res))
+    .catch(next);
+}
+
+export const allUsersAndFriended = ({ params, user }, res, next) => {
+  User.find().populate('badges', 'id points').populate('likes', 'id name').populate('language').then(users => {
+    return new Promise(function (res, rej) {
+      users.map((foundUser) => {
+        if (user.friends.length != 0) {
+          user.friends.forEach(userFriend => {
+            if (_.isEqual(userFriend.toString(), foundUser.id))
+            foundUser.set('friended', true)
+            else
+            foundUser.set('friended', false)
+          });
+        } else {
+          foundUser.set('friended', false)
+        }
+      });
+      res(users);
+    });
+  }).then(success(res)).catch(next);
+}
