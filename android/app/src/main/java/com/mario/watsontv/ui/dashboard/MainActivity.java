3536b66df8b6f39741195aedcd07c2e037df08fd
mariomrtsnz <mario.mrtsnz@gmail.com>
2019-03-11 22:47:25 +0100

    Fixed actionbar style. Started Dashboard styling.

--- android/app/src/main/java/com/mario/watsontv/ui/dashboard/MainActivity.java
+++ android/app/src/main/java/com/mario/watsontv/ui/dashboard/MainActivity.java
@@ -66,7 +66,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -78,9 +77,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
